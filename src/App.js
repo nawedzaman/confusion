7fe73ca9561bc1f587c206b4e19c616e67e7cae1
nawedzaman <zamannawed28@gmail.com>
2020-11-20 11:59:23 +0530

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Main from './components/MainComponent';
 import React ,{ Component } from 'react';
+import {  BrowserRouter } from "react-router-dom";
+
+
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div>
       <Main/>
     </div>
+    </BrowserRouter>
     );
   }
 }
