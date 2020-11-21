76f7659602e3b10ac7071ee54a85d600e6146d03
nawedzaman <zamannawed28@gmail.com>
2020-11-21 09:59:29 +0530

    Single Page Application Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import Dishdetail from './DishDetailComponent';
 
 class Main extends Component {
 
@@ -33,12 +34,20 @@ class Main extends Component {
         />
         );
     }
+
+    const DishWithId = ({match}) => {
+        return(
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        );
+      };
     return (
       <div>
         <Header/>
         <Switch>
             <Route path="/home" component={HomePage}/>
             <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
+            <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path= "/contactus" component={Contact}/>
             <Redirect to="/home"/>
         </Switch>
