fd2de65b74c1779f8890124f1f7f39a39a377cde
nawedzaman <zamannawed28@gmail.com>
2021-01-04 03:47:23 +0530

    redux thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -16,6 +16,7 @@ import {
 } from "reactstrap";
 import { LocalForm, Control, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
+import { Loading } from "./LoadingComponent";
 
 const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
@@ -159,7 +160,23 @@ function RenderComments({ comments, addComment, dishId }) {
 const DishDetails = (props) => {
   //destructure object
   //const {dish} =props ;
-  if (props.dish != null) {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else if (props.dish != null) {
     return (
       <div className="container">
         <div className="row">
